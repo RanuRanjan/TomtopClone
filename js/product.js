e90120ffb7d8fd8fe08a438fe94f1663c9063839
Ranu Ranjan <ranuranjan25@gmail.com>
2021-08-27 04:16:07 +0530

    all image change to and product almost finish

--- js/product.js
+++ js/product.js
@@ -1,89 +1,111 @@
 const productss = [
     {
       name: "Lenovo Laptop",
-      price: "10",
+      price: "$220",
+      price1:"$555",
       image: "https://img.tttcdn.com/product/xy/220/220/p/gu1/R/U/RTYSY-A10S-EU/RTYSY-A10S-EU-1-3b49-kDlM.jpg",
     },
 
     {
       name: "DJI Drone",
-      price: "20",
+      price: "$120",
+      price1:"$255",
       image: "https://img.tttcdn.com/product/xy/220/220/p/gu1/R/1/RM13901-1/RM13901-1-1-3ab0-hx1H.jpg",
     },
     {
-      name: "",
-      price: "30",
+      name: "Children Rgb Toys",
+      price: "$30",
+      price1:"$155",
       image: "https://img.tttcdn.com/product/xy/220/220/p/gu1/I/P/I1153P/I1153P-1-a553-Imc0.jpg",
     },
     {
       name: "Digital Analog Signal Conveter",
-      price: "60",
+      price: "$160",
+      price1:"$255",
       image: "https://img.tttcdn.com/product/xy/220/220/p/gu1/E/E0917/E0917-8-913e.jpg",
     },
     {
       name: "Nikon Camera Lens",
-      price: "40",
+      price: "$440",
+      price1:"$565",
+      price1:"$222",
       image: "https://img.tttcdn.com/product/xy/220/220/p/gu1/D/D1934/D1934-1-610a.jpg",
     },
     {
       name: "Tripod",
-      price: "50",
+      price: "$150",
+      price1:"$355",
       image: "https://img.tttcdn.com/product/xy/220/220/p/tt/i/6/i683-6-720d.jpg",
     },
     {
       name: "Smart Watch",
-      price: "50",
+      price: "$250",
+      price1:"$655",
       image: "https://img.tttcdn.com/product/xy/220/220/p/gu1/P/1/PB0246B-1/PB0246B-1-1-f34c-Sb8F.jpg",
     },
     {
       name: "Airpods",
-      price: "50",
+      price: "$150",
+      price1:"$1555",
       image: "https://img.tttcdn.com/product/xy/220/220/p/gu1/P/B/PAE1010B/PAE1010B-1-b34f-KQQd.jpg",
     },
     {
       name: "Room Temperature Checker",
-      price: "50",
+      price: "$50",
+      price1:"$150",
       image: "https://img.tttcdn.com/product/xy/220/220/p/gu1/H/1/H17031/H17031-1-5e6c-Vxq9.jpg",
     },
     {
       name: "Vaccum Cleaner",
-      price: "50",
+      price: "$350",
+      price1:"$255",
       image: "https://img.tttcdn.com/product/xy/220/220/p/gu1/E/U/E15305EU/E15305EU-1-cbc6-PsWP.jpg",
     },
     {
       name: "OWSOO 7 inch Wired Video Doorbell ",
-      price: "50",
+      price: "$29",
+      price1:"$65",
       image: "https://img.tttcdn.com/product/xy/377/377/p/gu1/S/U/S5890EU/S5890EU-1-bd19-xtDT.jpg",
     },
     {
       name: "Aluminum 2.5MPa 25KG Water Pressure",
-      price: "50",
+      price: "$55",
       image: "https://img.tttcdn.com/product/xy/377/377/p/gu1/E/0/E6590/E6590-1-5958-TkoV.jpg",
     },
     {
       name: "9Pcs Luxury Car Seat Covers Front +Back Seat",
-      price: "50",
+      price: "$89",
+      price1:"$470",
       image: "https://img.tttcdn.com/product/xy/377/377/p/gu1/K/A/K14824-A/K14824-A-1-4fd1-4DM2.jpg",
     },
     {
       name: "METERK 20V Cordless 10Inch Grass Trimmer",
-      price: "50",
+      price: "$29",
+      price1:"$355",
       image: "https://img.tttcdn.com/product/xy/377/377/p/gu1/E/U/E12755EU/E12755EU-1-64c6-Fg4K.jpg",
     },
     {
       name: "Retro Style Motorcycle Half Helmet",
-      price: "50",
+      price: "$17",
+      price1:"$105",
       image: "https://img.tttcdn.com/product/xy/377/377/p/gu1/K/B/K12521-B/K12521-B-1-de95-ASfP.jpg",
     },
     {
       name: "2pcs Table Folding Lifter Hydraulic Air Support",
-      price: "50",
+      price: "$39",
       image: "https://img.tttcdn.com/product/xy/377/377/p/gu1/E/6/E18990-26/E18990-26-1-85af-GdKv.jpg",
     },
     {
       name: "Y68 1.44in Intelligent Watches Heart Rate",
-      price: "50",
+      price: "$600",
+      price1:"$955",
       image: "https://img.tttcdn.com/product/xy/377/377/p/gu1/V/L/V9148NBL/V9148NBL-1-8810-BOBV.jpg",
+    },
+    {
+      name: "1800W Steam Mop Handheld Steam Cleaner",
+      price: "$80",
+      price1:"$355",
+      image: "https://img.tttcdn.com/product/xy/377/377/p/gu1/H/U/H35070EU/H35070EU-1-9192-J4K0.jpg",
     },
    
     
@@ -119,18 +141,24 @@ const productss = [
 
       p_price.innerText = product.price;
 
+      let p_price1 = document.createElement("span");
+
+      p_price1.innerText = product.price1;
+
       let image = document.createElement("img");
 
       image.src = product.image;
 
       let btn =document.createElement("button");
+       
+      let fav=document.createElement("img")
 
       btn.innerText="Add to cart"
       btn.onclick=function(){
         addtoCart(product);
       }
 
-      div.append(image, p_name, p_price,btn);
+      div.append(image, p_name, p_price,p_price1,btn,fav);
 
       data_div.append(div);
     });
